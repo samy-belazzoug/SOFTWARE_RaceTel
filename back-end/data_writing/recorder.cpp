4ee8311f7c400a87873ef9860be7d9b32dcae9dc
Samy Belazzoug <samy.belazzoug@laplateforme.io>
2026-02-16 09:50:50 +0100

    Basic structure

--- back-end/data_writing/recorder.cpp
+++ back-end/data_writing/recorder.cpp
@@ -5,11 +5,21 @@ int main() {
     SHME_physics p; 
     SHME_graphics g;
     p.Connect(); 
-    g.Connect();
+    g.Connect(); 
+
+    Circular_Buffer buffer;
+    buffer.frequency = 150;
+    buffer.item_size = sizeof(SPageFilePhysics);
+    buffer.seconds = 5;
+    std::cout << "Buffer size = " << buffer.item_size * buffer.frequency * buffer.seconds << std::endl;
+
+    std::cout << &p.physics << std::endl;
+
+
 
     while (g.graphics->status != AC_OFF) { // Assetto Corsa is launched
         if (g.graphics->status == AC_LIVE) { // Player is currently driving
-            std::cout << "fuel : " << p.physics->fuel  << " status : " << g.graphics->status << " rpms : " << p.physics->rpms << std::endl ;
+            std::cout << "fuel : " << p.physics->fuel  << " status : " << g.graphics->status << " rpms : " << p.physics->rpms << std::endl; 
         }
     }
     
--- back-end/data_writing/recorder.hpp
+++ back-end/data_writing/recorder.hpp
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include "../data_access/Assetto-Corsa/SHME_assetto_corsa.hpp"
 
-
+struct Circular_Buffer {
+    size_t item_size; // Size of one element, it can be a type, a struct etc..
+    size_t frequency; // In hz, for Assetto Corsa, it's 150hz
+    size_t seconds; // How much seconds you want to stock in the buffer
+    size_t read;
+    size_t write;
+};
 
 #endif
